35ee3bc93704bebbc5ab6f188aab581f1bb46fe7
wangpeie <wangpeie.enn.cn>
2021-01-28 18:09:30 +0800

    1. add lambda, match

--- src/main/scala/com/kd/PrintHello.scala
+++ src/main/scala/com/kd/PrintHello.scala
@@ -90,7 +90,53 @@ object PrintHello {
   }
 
 
+  /**
+   * 显示函数的各种lambda表达形式
+   */
+  def funLambda(): Unit = {
+    def foo(s: String) = {
+      println(s)
+    }
 
+    foo("你好!")
+
+    def hf(f: String => Unit) = f("higher")
+
+    hf(foo)
+
+    val f: String => Unit = foo
+    f("zaijian")
+
+    val f2: Function1[String, Unit] = foo
+    f2("f2 zaijian")
+
+    hf(f)
+
+    //开始lambda形式
+    println("----------lambda-------------------")
+    //第1个版本省略了s的类型，这是因为在定义hf时已经声明了参数类型为String=>Unit，编译器会把lambda表达式中的入参和出参按声明的类型来对待。
+    hf(s => println(s))
+    //第2个版本则是采用了占位符的方式。它的形式为，对于所有的 x=>g(x) 都可以用占位符的形式写为：g(_)，相当于省去了lambda表达式的入参和箭头部分，然后用占位符表示入参。
+    hf(println(_))
+    //第3个版本和第2个版本都是部分应用函数(partial applied function)的写法，当只有一个参数时，这两个写法是等价的。
+    hf(println _)
+    //第4个版本的背后其实是编译器支持lambda的“eta转换”
+    //简单的说就是对于lambda表达式中只有一个参数，并且箭头右边的逻辑是对入参执行一个函数：即 x => f(x)
+    //则可以简写为f
+    hf(println)
+  }
+
+  //match匹配
+  def matchFun(a: Int): Unit = {
+    println("----into matchFun----------")
+    var matchVar = a match {
+      case 1 => 101
+      case 2 => 102
+      case 3 => 103
+      case _ => 100
+    }
+    println(matchVar)
+  }
 
 
 
@@ -118,6 +164,15 @@ object PrintHello {
 
     val positon = 125
     println("floor[" + positon + "] = " + getFloorItem(positon))
+
+    //lambda
+    println("--------run funLambda-------------")
+    funLambda()
+
+
+    //match
+    matchFun(2)
+
   }
 
 
